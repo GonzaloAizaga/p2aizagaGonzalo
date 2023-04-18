694decf3a2ebb3fb3391224975612b79ce7babd5
GonzaloAizaga <129554571+GonzaloAizaga@users.noreply.github.com>
2023-04-18 19:09:31 -0300

    Delete TP1 GA directory

--- TP1 GA/js/funciones.js
+++
@@ -1,7 +0,0 @@
-console.log("funciona");
-function miFuncion(){
-    const apellido=document.getElementById("lastname");
-    const name=document.getElementById("name");
-    const age=document.getElementById("age");
-    alert(apellido.value+name.value+age.value);
-};

1a82f4722bd2f62d050535e35e5469213c668816
GonzaloAizaga <129554571+GonzaloAizaga@users.noreply.github.com>
2023-04-18 19:06:13 -0300

    Add files via upload

---
+++ TP1/js/funciones.js
@@ -0,0 +1,7 @@
+console.log("funciona");
+function miFuncion() {
+    const apellido = document.getElementById("lastname");
+    const name = document.getElementById("name");
+    const age = document.getElementById("age");
+    alert(apellido.value +" "+ name.value +" "+ age.value);
+};
